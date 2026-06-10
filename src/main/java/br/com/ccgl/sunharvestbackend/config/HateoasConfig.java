7e307f79908c8e6b6a5a4dc3d9d3db58f3672d9d
willahelmgui <ribeiro.guicosta@gmail.com>
2026-06-09 21:31:03 -0300

    Removendo objetos desnecessarios + adicionando configs

---
+++ src/main/java/br/com/ccgl/sunharvestbackend/config/HateoasConfig.java
@@ -0,0 +1,9 @@
+package br.com.ccgl.sunharvestbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+
+@Configuration
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class HateoasConfig {
+}
